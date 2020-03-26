f16353715da5fdf001b7e6c1527a03ff8f94b6e3
李旺理 <wangli.li@stringstech.co>
2020-03-26 15:02:52 +0800

    react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import AutoFocusInput  from './components/AutoFocusInput';
+import WrappedComponent   from './components/WrappedComponent';
 
 import './App.css';
 
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <AutoFocusInput  />
+      <WrappedComponent   />
     </div>
   );
 }
---
+++ src/store/state.js
@@ -0,0 +1,10 @@
+/**
+    此文件存放state默认值
+    同步数据：pageTitle
+    异步数据：infoList（将来用异步接口获取）
+ */
+ 
+export default {
+    pageTitle:"首页",
+    infoList:[]
+}
